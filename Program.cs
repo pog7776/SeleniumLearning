6c0dbf6f2f708b3ae690c88cb5b701044a0d5eb0
Jack Cooper <jack.cooper7776@gmail.com>
2022-10-23 00:44:19 +1100

    Testing. Currently can get a quote from my quotes page

--- Program.cs
+++ Program.cs
@@ -13,32 +13,102 @@ using WebDriverManager.DriverConfigs.Impl;
 
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
+using SeleniumDocs.GettingStarted;
+using System.Collections.Generic;
+using OpenQA.Selenium.Support.UI;
+using System.Threading;
 
 namespace SeleniumLearning {
     class Program {
         static void Main(string[] args) {
+            // FirstScriptTest test = new FirstScriptTest();
+            // test.CreateDriver();
+            // test.ChromeSession();
+            //Test();
+            PersonalSite();
+        }
+
+        protected static void Test() {
             //IWebDriver driver = new ChromeDriver();
 
             new DriverManager().SetUpDriver(new ChromeConfig());
             // Create Chrome driver
             ChromeDriver driver = new ChromeDriver();
+
             // Navigate to a webpage
             driver.Navigate().GoToUrl("https://www.selenium.dev/selenium/web/web-form.html");
+
             // Get the page title
             string title = driver.Title;
             Console.WriteLine(title);
+
             // Wait for page to load
             // NOTE - This is not a good practice just waiting but used to demonstrate
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromMilliseconds(500);
+
             // Find elements on the page
-            IWebElement textBox = driver.FindElement(By.Name("my-text"));
+            //IWebElement textBox = driver.FindElement(By.Name("my-text"));
+            IWebElement textBox = driver.FindElement(By.Id("my-text-id"));
             IWebElement submitButton = driver.FindElement(By.TagName("button"));
+
             // Type in the text box
             textBox.SendKeys("Selenium");
+
             // Click the submit button
             //submitButton.Click();
+
             // Close the browser
             //driver.Quit();
+        }
+
+        public static void PersonalSite() {
+            new DriverManager().SetUpDriver(new ChromeConfig());
+            ChromeDriver driver = new ChromeDriver();
+
+            //driver.Manage().Window.Maximize();
+
+            driver.Navigate().GoToUrl("https://jackcooper.dev");
+
+            driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(10);
+            driver.FindElement(By.Id("projects")).Click();
+
+            driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(10);
+            driver.FindElement(By.Id("quote")).Click();
+
+            driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(10);
+            IWebElement quote = driver.FindElement(By.Id("quoteContainer"));
+
+            WaitForStableText(quote);
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Quote: " + quote.Text);
+            Console.ResetColor();
+
+            driver.Quit();
+        }
+
+        public static bool WaitForStableText(IWebElement element, int pollRate = 5, int ttl = 5000) {
+            // Store previous text
+            string temp = "";
+            // Count attempts
+            int time = 0;
+
+            // INFO "..." is specifically for my site
+            while(element.Text != temp || element.Text == "...") {
+                // Update the temp text to the curreent text
+                temp = element.Text;
+
+                // Wait for time
+                Thread.Sleep(1000/pollRate);
+                time += 1000/pollRate;
+
+                // Check if gone over ttl
+                if(time >= ttl) {
+                    Console.WriteLine("No change detected.");
+                    return false;
+                }
+            }
+            return true;
         }
     }
 }
---
+++ example.cs
@@ -0,0 +1,50 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace SeleniumDocs.GettingStarted
+{
+    [TestClass]
+    public class FirstScriptTest
+    {
+
+        protected IWebDriver driver;
+
+        [TestInitialize]
+        public void CreateDriver()
+        {
+            new DriverManager().SetUpDriver(new ChromeConfig());
+            driver = new ChromeDriver();
+        }
+
+        [TestCleanup]
+        public void QuitDriver()
+        {
+            driver.Quit();
+        }
+
+        [TestMethod]
+        public void ChromeSession()
+        {
+            driver.Navigate().GoToUrl("https://www.selenium.dev/selenium/web/web-form.html");
+
+            var title = driver.Title;
+            Assert.AreEqual("Web form", title);
+
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromMilliseconds(500);
+
+            var textBox = driver.FindElement(By.Name("my-text"));
+            var submitButton = driver.FindElement(By.TagName("button"));
+            
+            textBox.SendKeys("Selenium");
+            //submitButton.Click();
+            
+            var message = driver.FindElement(By.Id("message"));
+            var value = message.Text;
+            Assert.AreEqual("Received!", value);
+        }
+    }
+}
