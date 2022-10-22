8119082078af031fc2b78bf283b82d6911dd9367
Jack Cooper <jack.cooper7776@gmail.com>
2022-10-23 02:03:04 +1100

    Testing on each platform

--- Program.cs
+++ Program.cs
@@ -4,9 +4,8 @@ using System.Linq;
 
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-// using OpenQA.Selenium.Edge;
-// using OpenQA.Selenium.Firefox;
-// using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
 
 using WebDriverManager;
 using WebDriverManager.DriverConfigs.Impl;
@@ -25,8 +24,105 @@ namespace SeleniumLearning {
             // test.CreateDriver();
             // test.ChromeSession();
             //Test();
-            PersonalSite();
+
+            DriverManager dm = new DriverManager();
+
+            // ChromeDriver driver = new ChromeDriver();
+            // Console.WriteLine(driver.GetType());
+            //PersonalSite(driver);
+
+            foreach(WebDriver driver in SetupDrivers(dm)) {
+                driver.Manage().Window.Maximize();
+
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("Testing: " + driver.GetType());
+                Console.ResetColor();
+
+                try {
+                    PersonalSite(driver);
+                }
+                catch (Exception e){
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(driver.GetType() + " Failed");
+                    Console.WriteLine("Error:" + e);
+                    Console.ResetColor();
+                    driver.Quit();
+                }
+            }
         }
+
+        public static List<WebDriver> SetupDrivers(DriverManager dm) {
+            List<WebDriver> drivers = new List<WebDriver>();
+
+            dm.SetUpDriver(new ChromeConfig());
+            drivers.Add(new ChromeDriver());
+
+            dm.SetUpDriver(new FirefoxConfig());
+            drivers.Add(new FirefoxDriver());
+            
+            dm.SetUpDriver(new EdgeConfig());
+            drivers.Add(new EdgeDriver());
+
+            drivers.ForEach(driver => {
+                driver.Manage().Window.Minimize();
+                Console.WriteLine("Created: " + driver.GetType());
+            });
+
+            return drivers;
+        }
+
+        public static void PersonalSite(WebDriver driver) {
+            driver.Navigate().GoToUrl("https://jackcooper.dev");
+
+            driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(20);
+            driver.FindElement(By.Id("projects")).Click();
+
+            driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(20);
+            driver.FindElement(By.Id("quote")).Click();
+
+            driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(20);
+            IWebElement quote = driver.FindElement(By.Id("quoteContainer"));
+
+            WaitForStableText(quote, 0.5f);
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Quote: " + quote.Text);
+            Console.ResetColor();
+
+            driver.Quit();
+        }
+
+        /// <summary>
+        /// Polls an elements text until it hasn't changed since last poll.
+        /// </summary>
+        /// <param name="element">The element to watch for changes.</param>
+        /// <param name="pollRate">Times per second to check the element.</param>
+        /// <param name="ttl">Time in ms until it determines the text is not stable.</param>
+        /// <returns>true: When no change is detected since last poll. false: When ttl is exceeded.</returns>
+        public static bool WaitForStableText(IWebElement element, float pollRate = 5, int ttl = 5000) {
+            // Store previous text
+            string previous = "";
+            // Count attempts
+            int time = 0;
+
+            // While no changes
+            while(element.Text != previous) {
+                // Update the temp text to the curreent text
+                previous = element.Text;
+
+                // Wait for time
+                Thread.Sleep(Convert.ToInt32(1000/pollRate));
+                time += Convert.ToInt32(1000/pollRate);
+
+                // Check if gone over ttl
+                if(time >= ttl) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+
 
         protected static void Test() {
             //IWebDriver driver = new ChromeDriver();
@@ -59,62 +155,6 @@ namespace SeleniumLearning {
 
             // Close the browser
             //driver.Quit();
-        }
-
-        public static void PersonalSite() {
-            new DriverManager().SetUpDriver(new ChromeConfig());
-            ChromeDriver driver = new ChromeDriver();
-
-            //driver.Manage().Window.Maximize();
-
-            driver.Navigate().GoToUrl("https://jackcooper.dev");
-
-            driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(10);
-            driver.FindElement(By.Id("projects")).Click();
-
-            driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(10);
-            driver.FindElement(By.Id("quote")).Click();
-
-            driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(10);
-            IWebElement quote = driver.FindElement(By.Id("quoteContainer"));
-
-            WaitForStableText(quote, 0.5f);
-
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("Quote: " + quote.Text);
-            Console.ResetColor();
-
-            driver.Quit();
-        }
-
-        /// <summary>
-        /// Polls an elements text until it hasn't changed since last poll
-        /// </summary>
-        /// <param name="element">The element to watch for changes.</param>
-        /// <param name="pollRate">Times per second to check the element.</param>
-        /// <param name="ttl">Time in ms until it determines the text is not stable.</param>
-        /// <returns>true: When no change is detected since last poll. false: When ttl is exceeded.</returns>
-        public static bool WaitForStableText(IWebElement element, float pollRate = 5, int ttl = 5000) {
-            // Store previous text
-            string temp = "";
-            // Count attempts
-            int time = 0;
-
-            // INFO "..." is specifically for my site
-            while(element.Text != temp) {// || element.Text == "...") {
-                // Update the temp text to the curreent text
-                temp = element.Text;
-
-                // Wait for time
-                Thread.Sleep(Convert.ToInt32(1000/pollRate));
-                time += Convert.ToInt32(1000/pollRate);
-
-                // Check if gone over ttl
-                if(time >= ttl) {
-                    return false;
-                }
-            }
-            return true;
         }
     }
 }
